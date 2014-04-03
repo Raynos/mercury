c3b52ed3cdc8f7c073fad1fc433d154693e2a944
ben hockey <neonstalwart@gmail.com>
2014-04-04 00:34:31 +0800

    simplify EventRouter and remove geval dependency

--- examples/todomvc/input.js
+++ examples/todomvc/input.js
@@ -1,6 +1,5 @@
 var window = require("global/window")
 var HashRouter = require("hash-router")
-var Event = require("geval")
 
 var mercury = require("../../index.js")
 
@@ -19,10 +18,10 @@ function createInput() {
 }
 
 function EventRouter() {
-    var router = HashRouter()
+    var router = HashRouter(),
+        hash = mercury.value()
     window.addEventListener("hashchange", router)
 
-    return Event(function (emit) {
-        router.on("hash", emit)
-    })
+    router.on("hash", hash.set)
+    return hash
 }
