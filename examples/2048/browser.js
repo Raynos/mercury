2b62170e2edc17bbe007b70073b8eb5b3eb40735
Raynos <raynos2@gmail.com>
2014-04-17 01:14:24 -0700

    Input, State, Update, Render boilerplate

--- examples/2048/browser.js
+++ examples/2048/browser.js
@@ -1,6 +1,9 @@
 var mercury = require("../../index.js")
 
+var Input = require("./input.js")
 var Render = require("./render.js")
+var State = require("./state.js")
+var Update = require("./update.js")
 
 module.exports = createApp
 
@@ -10,8 +13,14 @@ mercury.app(document.body, state, Render)
 function createApp() {
     var initialState = null
 
-    var events = null
-    var state = mercury.hash({})
+    var events = Input()
+    var state = State.game(events, initialState)
+
+    wireUpEvents(state, events)
 
     return state
+}
+
+function wireUpEvents(state, events) {
+    // events.foo(Update.foo.bind(null, state))
 }
