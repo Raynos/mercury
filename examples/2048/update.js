9dfa06b75e77e200ba6ec8c541ec3d710745abf0
Raynos <raynos2@gmail.com>
2014-04-17 02:13:58 -0700

    added more implementation in progress

--- examples/2048/update.js
+++ examples/2048/update.js
@@ -1,3 +1,30 @@
 module.exports = {
-    
+    resetGame: resetGame,
+    move: move,
+    updateScore: updateScore,
+    movesAvailable: movesAvailable
+}
+
+function resetGame(state) {
+
+}
+
+function move(state, data) {
+
+}
+
+function updateScore(state) {
+
+}
+
+function movesAvailable(state) {
+    return cellsAvailable(state) || tileMatchesAvailable(state)
+}
+
+function cellsAvailable(state) {
+
+}
+
+function tileMatchesAvailable(state) {
+
 }
