cd166e52b9682f55a09b75ff9a782f8e9f5172aa
Raynos <raynos2@gmail.com>
2014-04-17 02:13:58 -0700

    added more implementation in progress

--- examples/2048/state.js
+++ examples/2048/state.js
@@ -2,18 +2,47 @@ var mercury = require("../../index.js")
 var extend = require("xtend")
 
 var Game2048 = {
-    size: 4
+    size: 4,
+    grid: [],
+    currentScore: 0,
+    highScore: 0
+}
+
+var GameTile = {
+    number: 2,
+    x: -1,
+    y: -1
 }
 
 module.exports = {
-    game: GameState
+    game: gameBoard,
+    tile: numberTile
 }
 
-function GameState(events, initialState) {
+function gameBoard(events, initialState) {
     var state = extend(Game2048, initialState)
+
+    if (state.grid.length === 0) {
+        for (var i = 0; i < state.size * state.size; i++) {
+            state.grid[i] = null
+        }
+    }
 
     return mercury.hash({
         events: events,
-        size: state.size
+        grid: mercury.array(state.grid),
+        size: mercury.value(state.size),
+        currentScore: mercury.value(state.currentScore),
+        highScore: mercury.value(state.highScore)
+    })
+}
+
+function numberTile(tile) {
+    var state = extend(GameTile, tile)
+
+    return mercury.hash({
+        number: mercury.value(state.number),
+        x: mercury.value(state.x),
+        y: mercury.value(state.y)
     })
 }
