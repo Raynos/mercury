8e79e4d8013cd452d698dcdd05148692860591f0
Raynos <raynos2@gmail.com>
2014-05-03 21:12:40 -0700

    added move logic

--- examples/2048/input.js
+++ examples/2048/input.js
@@ -1,9 +1,15 @@
+var Keyboard = require("frp-keyboard")
+var merge = require("./lib/observ-merge.js")
+
 var mercury = require("../../index.js")
 
 module.exports = createInput
 
 function createInput() {
+    var keyboard = Keyboard()
     var events = mercury.input(["resetGame"])
+
+    events.move = merge([keyboard.wasd, keyboard.arrows])
 
     return events
 }
