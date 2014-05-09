c6f37f2f7b7bd63ef8d61a799cb4dd55b9b9e240
Matt Esch <matt@mattesch.info>
2014-05-08 22:47:36 -0700

    add transition hook

---
+++ examples/2048/lib/transition.js
@@ -0,0 +1,30 @@
+var nextTick = require("next-tick")
+
+module.exports = SetNext
+
+function Transition(value, propName) {
+    if (!(this instanceof Transition)) {
+        return new Transition(value)
+    }
+
+    this.value = value
+    this.property = propName
+}
+
+Transition.prototype.hook = function setOnNextTick(domNode, propName) {
+    var value = this.value
+    var property = this.property
+
+    if (property === null || property === undefined) {
+        property = propName
+    }
+
+    if (domNode[property]) {
+        nextTick(function () {
+            console.log(domNode, propName)
+            domNode[property] = value
+        })
+    } else {
+        domNode[property] = value
+    }
+}
