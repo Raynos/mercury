b8752b476fe75c392a05cf602f52e3d9ae56c061
Raynos <raynos2@gmail.com>
2014-05-31 15:02:57 -0400

    added proper dist sript

---
+++ bin/dist.js
@@ -0,0 +1,20 @@
+var browserify = require('browserify');
+var fs = require('fs');
+var path = require('path');
+
+var version = require('../package.json').version;
+
+var bundle = browserify()
+    .add(path.join(__dirname, '..', 'index.js'))
+    .bundle({ standalone: 'mercury' })
+
+var dest = fs.createWriteStream(
+    path.join(__dirname, '..', 'dist', 'mercury.js')
+)
+
+dest.write('// mercury @ ' + version + ' \n');
+bundle.pipe(dest);
+
+dest.on('finish', function () {
+    console.log('OK done')
+})
