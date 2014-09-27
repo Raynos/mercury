5b0b8d190fba5b2fa20608fd61b80e55b29fb1ab
omphalos <omph.alos@yahoo.com>
2014-09-27 17:21:43 -0500

    add comments and tests to time-travel.

--- time-travel.js
+++ time-travel.js
@@ -2,40 +2,63 @@ module.exports = TimeTravel
 
 function TimeTravel(state) {
 
-    var states = [state()]
-    var cursor = 0
-    var transitioning = false
+    var history = [state()]
 
-    state(function (newState) {
-        if(transitioning) {
+    // Tracks the current position in history.
+    var cursor = 0
+
+    var isRedoOrUndo = false
+
+    state(function recordState(newState) {
+
+        // This function gets called whenever there is a state change.
+        // State changes happen due to events being handled, or due to
+        // undo/redo.
+
+        // If we are replaying items in the history,
+        // we don't want to re-add them to the end of the history.
+        // Just quit.
+        if(isRedoOrUndo) {
             return
         }
-        states.splice(cursor + 1)
-        states.push(newState)
-        cursor = states.length - 1
+
+        // If we've made it this far, `newState` is due to a new action,
+        // not due to undo/redo.
+
+        // If we've called `undo` a bunch of times,
+        // the cursor won't be at the end.
+        // Any states past the cursor should be cut off.
+        history.splice(cursor + 1)
+
+        // Add the new item to the history
+        history.push(newState)
+
+        cursor = history.length - 1
     })
 
     return { undo: undo, redo: redo }
 
     function undo() {
         if (cursor < 1) {
+            // Don't move before the beginning of time
             return
         }
         cursor--
-        transitioning = true
-        state.set(states[cursor])
-        transitioning = false
-        return states[cursor]
+        isRedoOrUndo = true
+        state.set(history[cursor])
+        isRedoOrUndo = false
+        return history[cursor]
     }
 
     function redo() {
-        if(cursor + 1 >= states.length) {
+        if(cursor + 1 >= history.length) {
+            // Don't move past the end of time
             return
         }
         cursor++
-        transitioning = true
-        state.set(states[cursor])
-        transitioning = false
-        return states[cursor]
+        isRedoOrUndo = true
+        state.set(history[cursor])
+        isRedoOrUndo = false
+        return history[cursor]
     }
 }
