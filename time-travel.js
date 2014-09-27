d0c0d6947e6e76f5d810f526dcb9ecd23044fa62
omphalos <omph.alos@yahoo.com>
2014-09-27 11:44:11 -0500

    add full history undo/redo to time-travel.js

--- time-travel.js
+++ time-travel.js
@@ -1,23 +1,41 @@
 module.exports = TimeTravel
 
 function TimeTravel(state) {
+
     var states = [state()]
+    var cursor = 0
+    var transitioning = false
 
     state(function (newState) {
-        if (newState !== states[0]) {
-            states.unshift(newState)
-        }
-    })
-
-    return undo
-
-    function undo() {
-        if (states.length <= 1) {
+        if(transitioning) {
             return
         }
+        states.splice(cursor + 1)
+        states.push(newState)
+        cursor = states.length - 1
+    })
 
-        states.shift()
-        state.set(states[0])
-        return states[0]
+    return { undo: undo, redo: redo }
+
+    function undo() {
+        if (cursor < 1) {
+            return
+        }
+        cursor--
+        transitioning = true
+        state.set(states[cursor])
+        transitioning = false
+        return states[cursor]
+    }
+
+    function redo() {
+        if(cursor + 1 >= states.length) {
+            return
+        }
+        cursor++
+        transitioning = true
+        state.set(states[cursor])
+        transitioning = false
+        return states[cursor]
     }
 }
