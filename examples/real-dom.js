2a7d8a35f64d169f9ab14745e6f156cd6ce4dbc9
Arve Knudsen <arve.knudsen@gmail.com>
2015-06-08 17:51:46 +0200

    Add widget to real-dom example

--- examples/real-dom.js
+++ examples/real-dom.js
@@ -8,11 +8,26 @@ var setTimeout = require('timers').setTimeout;
 function Hook() {}
 // Hook to be called when DOM node has been rendered
 Hook.prototype.hook = function hook(node) {
-    node.innerText = 'This is set directly on the real DOM node.';
+    node.innerHTML = 'Set directly on real DOM node, by <em>hook</em>.';
 };
 // Hook to be called when DOM node is removed
 Hook.prototype.unhook = function unhook(node) {
-    node.innerText = 'Unhook hook called';
+    node.innerHTML = 'Set directly on real DOM node, by <em>unhook</em>.';
+};
+
+function Widget() {
+    this.type = 'Widget';
+}
+
+Widget.prototype.init = function init() {
+    var elem = document.createElement('div');
+    elem.innerHTML = 'Set directly on real DOM node, by widget ' +
+        '<em>before</em> update.';
+    return elem;
+};
+Widget.prototype.update = function update(prev, elem) {
+    elem.innerHTML = 'Set directly on real DOM node, by widget ' +
+        '<em>after</em> update.';
 };
 
 function App() {
@@ -26,13 +41,16 @@ function App() {
 }
 
 App.render = function App(state) {
-    return !state.remove ?
-        // Create virtual node with hook
-        h('div', {
-            hook: new Hook()
-        }) :
-        // Node without hook
-        h('div');
+    return h('div', [
+        !state.remove ?
+            // Create virtual node with hook
+            h('div', {
+                hook: new Hook()
+            }) :
+            // Node without hook
+            h('div'),
+        new Widget()
+    ]);
 };
 
 hg.app(document.body, App(), App.render);
