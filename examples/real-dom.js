b933d1be52db5e31e313558e389a9a299f8386cf
Arve Knudsen <arve.knudsen@gmail.com>
2015-06-09 12:07:18 +0200

    Only modify attributes in virtual-dom hook

--- examples/real-dom.js
+++ examples/real-dom.js
@@ -8,11 +8,13 @@ var setTimeout = require('timers').setTimeout;
 function Hook() {}
 // Hook to be called when DOM node has been rendered
 Hook.prototype.hook = function hook(node) {
-    node.innerHTML = 'Set directly on real DOM node, by <em>hook</em>.';
+    // Note that you should only modify DOM node attributes in virtual-dom
+    // hooks, anything else is unsafe
+    node.style.color = 'red';
 };
 // Hook to be called when DOM node is removed
 Hook.prototype.unhook = function unhook(node) {
-    node.innerHTML = 'Set directly on real DOM node, by <em>unhook</em>.';
+    node.style.color = null;
 };
 
 function Widget() {
@@ -21,34 +23,35 @@ function Widget() {
 
 Widget.prototype.init = function init() {
     var elem = document.createElement('div');
-    elem.innerHTML = 'Set directly on real DOM node, by widget ' +
+    elem.innerHTML = 'Content set directly on real DOM node, by widget ' +
         '<em>before</em> update.';
     return elem;
 };
 Widget.prototype.update = function update(prev, elem) {
-    elem.innerHTML = 'Set directly on real DOM node, by widget ' +
+    elem.innerHTML = 'Content set directly on real DOM node, by widget ' +
         '<em>after</em> update.';
 };
 
 function App() {
     var state = hg.state({
-        remove: hg.value(false)
+        updated: hg.value(false)
     });
     setTimeout(function timer() {
-        state.remove.set(true);
+        state.updated.set(true);
     }, 2000);
     return state;
 }
 
 App.render = function App(state) {
     return h('div', [
-        !state.remove ?
-            // Create virtual node with hook
-            h('div', {
-                hook: new Hook()
-            }) :
-            // Node without hook
-            h('div'),
+        // Create virtual node with hook if !state.updated
+        h('div', !state.updated ? {hook: new Hook()} : {}, [
+            'Style attribute of real DOM node set by ',
+            h('em', 'hook'),
+            ' and unset by ',
+            h('em', 'unhook')
+        ]),
+        // Create widget controlled node
         new Widget()
     ]);
 };
